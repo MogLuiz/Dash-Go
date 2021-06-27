544d18df0ba82f03af9ffc830c96a219a4324aaf
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-06-27 13:00:53 -0300

    font setting

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -15,6 +15,10 @@ export const theme = extendTheme({
             '900': '#181b23',
           },
     },
+    fonts: {
+        heading: 'Roboto',
+        body: 'Roboto'
+    },
     styles: {
         global: {
             body: {
