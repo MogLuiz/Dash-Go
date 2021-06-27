c338d40989844b3e508ab18bc85279200dd21458
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-06-27 17:42:35 -0300

    Finishing the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
     return (
@@ -48,6 +48,44 @@ export function Header() {
 
               <Icon as={RiSearchLine} fontSize="20"/>  
             </Flex>
+       
+            <Flex
+              align="center"
+              marginLeft="auto"
+            >
+
+              <HStack 
+                spacing="8"
+                mx="8"
+                pr="8"
+                py="1"
+                color="gray.300"
+                borderRightWidth={1}
+                borderColor="gray.700"
+              >
+
+                <Icon as={RiNotificationLine} fontSize="20"/>
+                <Icon as={RiUserAddLine} fontSize="20"/>
+
+              </HStack>
+
+              <Flex align="center">
+                <Box mr="4" textAlign="right">
+                    <Text>Luiz Henrique</Text>
+                    <Text 
+                      color="gray.300" 
+                      fontSize="small"
+                    >
+                      luizhjramos@outlook.com
+                    </Text>
+                </Box>
+
+                <Avatar size="md" name="Luiz Henrique" src="https://github.com/MogLuiz.png"/>
+
+              </Flex>
+
+            </Flex>
+       
         </Flex>
     )
 }
