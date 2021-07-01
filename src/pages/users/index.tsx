802b1c13be8881ab2774dc81ff1b22ed017a3e1e
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-07-01 16:57:24 -0300

    Paging Responsiveness

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,17 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Text, Td } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Text, Td, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+
+    const isWideVersion = useBreakpointValue ({
+        base: false,
+        lg: true,
+    })
+
     return (
         <Box>
             <Header />
@@ -31,17 +38,17 @@ export default function UserList() {
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" width="8">
+                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                                     <Checkbox colorScheme="pink"/>
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de Cadastro</Th>
-                                <Th width="8"></Th>
+                                { isWideVersion && <Th>Data de Cadastro</Th>}
+                               
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Checkbox colorScheme="pink"/>
                                 </Td>
                                 <Td>
@@ -50,18 +57,8 @@ export default function UserList() {
                                         <Text fontSize="small" color="gray.300">luizhjramos@outlook.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>09 de abril de 2000</Td>
-                                <Td>
-                                <Button 
-                                    as="a"
-                                    size="sm"
-                                    fontSize="sm"
-                                    colorScheme="pink"
-                                    leftIcon={ <Icon as={RiPencilLine} fontSize="16"/>}
-                                >
-                                  Editar
-                                </Button>
-                                </Td>
+                                { isWideVersion && <Td>09 de abril de 2000</Td>}
+                                
                             </Tr>
                         </Tbody>
                     </Table>
