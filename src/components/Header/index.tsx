85dfaaf5ec61f053726158c3a9128f8cec5ff60c
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-07-01 09:14:59 -0300

    SearchBox responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -25,7 +25,7 @@ export function Header() {
             
             <Logo />
 
-            <SearchBox />
+            { isWideVersion && <SearchBox /> }
        
             <Flex
               align="center"
