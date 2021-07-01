57afd2632fcc932575621b7e6bff09d0e04f8f86
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-07-01 09:07:23 -0300

    responsiveness profile name and email

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,24 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
 
-export function Profile() {
+interface IProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: IProfileProps) {
     return (
         <Flex align="center">
-              <Box mr="4" textAlign="right">
-                    <Text>Luiz Henrique</Text>
-                    <Text 
-                      color="gray.300" 
-                      fontSize="small"
-                    >
-                      luizhjramos@outlook.com
-                    </Text>
-              </Box>
+              
+              { showProfileData && ( // se for verdadeiro vai renderizar
+                <Box mr="4" textAlign="right">
+                  <Text>Luiz Henrique</Text>
+                  <Text 
+                    color="gray.300" 
+                    fontSize="small"
+                >
+                    luizhjramos@outlook.com
+                  </Text>
+                </Box>
+              )}
 
             <Avatar size="md" name="Luiz Henrique" src="https://github.com/MogLuiz.png"/>
 
