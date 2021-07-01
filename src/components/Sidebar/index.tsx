dbec9f347de5ff01cd09b1226eccf97ccb2f1a86
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-06-30 22:24:48 -0300

    responsiveness logo

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -6,7 +6,7 @@ import { NavSection } from './NavSection'
 
 export function Sidebar() {
     return (
-        <Box as="aside" w="64" mr="8">
+        <Box as="aside" w="64" mr="8" >
             <Stack spacing="12" align="flex-start">
 
                 <NavSection title="GERAL">
