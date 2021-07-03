aa6eb51f2673bedef5cb167adf647c15dd069133
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-07-03 13:21:51 -0300

    signaling active link

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -4,16 +4,21 @@ import { cloneElement, ReactElement } from 'react'
 
 interface IActiveLinkProps extends LinkProps {
     children: ReactElement;
+    shouldMatchExactHref?: boolean;
 }
 
-export function ActiveLink({ children, ...rest }: IActiveLinkProps) {
+export function ActiveLink({ children, shouldMatchExactHref = false, ...rest }: IActiveLinkProps) {
 
     const { asPath } = useRouter()
 
     let isActive = false
 
-    if (asPath === rest.href || asPath === rest.as) {
+    if ( shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) {
         isActive = true
+    }
+
+    if ( !shouldMatchExactHref && (asPath.startsWith(String(rest.href)) || asPath.startsWith(String(rest.as)))) {
+        isActive = true 
     }
 
     return (
